490222ab9f1d3423e1b4d3b25758adc81da1f022
Kunal Sharma <kunal0199@gmail.com>
2022-07-12 11:04:52 +0530

    Post entitiy created

---
+++ src/main/java/com/haanbhai/blogs/entities/Post.java
@@ -0,0 +1,28 @@
+package com.haanbhai.blogs.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer postId;
+    @Column(name = "title", nullable = false, length = 100)
+    private String postTitle;
+    @Column(length = 1000)
+    private String content;
+    private String imageName;
+    private Date postCreated;
+
+    private Category category;
+    private User user;
+}
